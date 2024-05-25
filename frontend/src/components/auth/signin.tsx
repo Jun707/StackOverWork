39ad19780144412210c3b9aff11b909ce5ef9b41
Jun707 <juntowork@gmail.com>
2024-05-24 19:24:25 -0600

    Added signin and sign out for frontend

---
+++ frontend/src/components/auth/signin.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+
+export default function SignIn() {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const formData = new FormData(e.currentTarget);
+
+        const body = {
+            username: formData.get("username"),
+            email: formData.get("email"),
+            password: formData.get("password"),
+        };
+
+        const res = await fetch('/api/signin', {
+            method:"POST",
+            body: JSON.stringify(body),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+
+        await res.json()
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username"> Username </label>
+                <input type="text" name="username"></input>
+                <label htmlFor="email"> Email </label>
+                <input type="text" name="email"></input>
+                <label htmlFor="password"> Password </label>
+                <input type="password" name="password"></input>
+                <button type="submit"> Sign in </button>
+            </form>
+
+        </div>
+    )
+}
